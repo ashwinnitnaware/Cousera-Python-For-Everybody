7a5e2e34ba988fe4f4139c088d3fe4df0db35cbd
Ashwin Nitnaware <44764455+ashwinnitnaware@users.noreply.github.com>
2020-07-05 10:52:32 +0530

    Update Chapter 1.py

--- Programming for Everybody (Getting Started with Python)/Chapter 1.py
+++ Programming for Everybody (Getting Started with Python)/Chapter 1.py
@@ -1,5 +1,5 @@
-Chapter 1
-Write a program that uses a print statement to say 'hello world' as shown in 'Desired Output'
+#Chapter 1
+#Write a program that uses a print statement to say 'hello world' as shown in 'Desired Output'
 
 # the code below almost works
 print("hello world")
