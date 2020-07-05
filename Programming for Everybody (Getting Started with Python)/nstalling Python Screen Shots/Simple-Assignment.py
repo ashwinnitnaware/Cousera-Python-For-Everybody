8b66865138d722951811582eeceb83339b6c90b8
Ashwin Nitnaware <44764455+ashwinnitnaware@users.noreply.github.com>
2020-07-05 10:46:52 +0530

    Add files via upload

---
+++ Programming for Everybody (Getting Started with Python)/nstalling Python Screen Shots/Simple-Assignment.py
@@ -0,0 +1,9 @@
+#Practice Peer-graded Assignment: Optional- Installing Python Screen Shots
+
+#Simple Print Assignment
+
+#1.png - Please upload an image of text editor showing your Python code with single print statement, using anything except
+
+#2.png - Please upload an image showing your Python code running in the command line or terminal. Please include both the python command as well as the output from the program.
+
+
